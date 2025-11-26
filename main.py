c72862ce8755f97265f10ddfaffe03ba5c642be1
wiwisnouk <wiwiskouk@gmail.com>
2025-11-26 23:27:59 +0300

    requir changes

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 from fastapi import FastAPI, HTTPException, Form
-from routers import HomePage
+from routers.HomePage import router
 
 app = FastAPI()
 
-app.include_router(HomePage.router)
+app.include_router(router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
